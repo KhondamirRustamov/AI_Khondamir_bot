b83c8d3422ded7dc793ef089e97b4d7373d7c2cb
KhondamirRustamov <69417652+KhondamirRustamov@users.noreply.github.com>
2021-03-27 20:18:26 +0500

    Add files via upload

---
+++ telebotus.py
@@ -0,0 +1,63 @@
+import random
+import json
+
+import torch
+
+from model import NeuralNet
+from nltk_modules import bag_of_words, tokenize
+
+import telebot
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+
+bot = telebot.TeleBot('1693467094:AAFIioioYeEdXTyvmIm9vd3VGeN7VyYvuiQ')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    sentence = tokenize(message.text.lower())
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                message1 = random.choice(intent['responses'])
+                bot.send_message(message.chat.id, message1)
+    else:
+        message1 = 'Я вас не понял...'
+        bot.send_message(message.chat.id, message1)
+        
+bot.polling()
+
+
+
